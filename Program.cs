afa2aac7140f26200488c562f60d9b05b871d4b1
Nikembi Travis <nikembitravis@outlook.com>
2024-09-16 22:38:34 -0500

    Init push

--- Program.cs
+++ Program.cs
@@ -1,27 +1,34 @@
 ﻿using System;
+using System.Linq.Expressions;
+using System.Runtime.ExceptionServices;
+using Week2.Entities;
 
 namespace Week2
 {
     internal class Program
     {
+        static bool exit;
+        static List<Contacts> contacts;
         static void Main(string[] args)
         {
-            bool exit = false;
+
             while (!exit)
             {
                 DisplayMenu();
+
             }
            
         }
 
         static void DisplayMenu()
         {
-            Console.WriteLine("Choose an option:");
+            Console.WriteLine($"Choose an option:{Environment.NewLine}");
             Console.WriteLine("1. Search Contacts");
-            Console.WriteLine("2. Create Contact");
-            Console.WriteLine("3. View Contacts/PhoneBook");
-            Console.Write("Enter your choice: ");
-
+            Console.WriteLine("2. Add New Contact");
+            Console.WriteLine("3. Contacts & Phonebook");
+            Console.WriteLine($"4. Exit{Environment.NewLine}");
+            Console.Write("Choose Option: ");
+            
             if (int.TryParse(Console.ReadLine(), out int menuOption))
             {
                 switch (menuOption)
@@ -30,20 +37,38 @@ namespace Week2
                         Console.WriteLine("You selected: Search Contacts");
                         break;
                     case 2:
-                        Console.WriteLine("You selected: Create Contact");
+                        Create();
                         break;
                     case 3:
                         Console.WriteLine("You selected: View Contacts/PhoneBook");
                         break;
+                    case 4:
+                        Exit();
+                        break;
                     default:
-                        Console.WriteLine("Invalid choice. Please enter 1, 2, or 3.");
+                        Console.WriteLine("Invalid input. Please enter a valid option.");
                         break;
                 }
             }
-            else
-            {
-                Console.WriteLine("Invalid input. Please enter a valid number.");
-            }
+        }
+        static void Create()
+        {
+            Console.WriteLine($"New Contact{Environment.NewLine}-----------");
+            contacts.Add
+                (new Contacts
+                {
+                    FirstName = Console.ReadLine(),
+                    LastName = Console.ReadLine(),
+                    PhoneNumber = Convert.ToInt32(Console.ReadLine())
+                });
+                
+            
+        }
+        static void Exit()
+        {
+            exit = true;
+            Console.WriteLine($"{Environment.NewLine}Good Bye! Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
